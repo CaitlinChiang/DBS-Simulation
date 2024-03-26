842e56cb09eabb134cb89b6435fe3b110f3a8ed5
caitlin.ai/ <chiangcaitlin2003@gmail.com>
2024-03-26 14:47:09 +0800

    Constructed returnLaterQueueNumberManager

--- src/simulation/returnLaterQueueNumberManager.ts
+++ src/simulation/returnLaterQueueNumberManager.ts
@@ -1,14 +1,43 @@
 import { Customer } from '../types/customer'
-import { State } from '../enums/states'
+import { State, EventState } from '../enums/states'
+import { StateFromReturnLaterQNProb, StateFromMissingQNProb } from '../enums/probabilities'
+import { Station } from '../enums/station'
+import { returnResultBasedOnProb } from '../utils/returnResultBasedOnProb'
+import { stationManager } from './stationManager'
+import { updateCustomerDwellTimeAndExitSimulation } from '../utils/updateCustomerDwellTimeAndExitSimulation'
 
 class ReturnLaterQueueNumberManager {
-  private returnLaterQueueNumberQueue: Customer[] = []
-
   constructor() {}
 
   appendCustomerToReturnLaterQueueNumberQueue(customer: Customer) {
     customer.state = State.RETURN_LATER_QN
-    this.returnLaterQueueNumberQueue.push(customer)
+    this.handleCustomerActionFromStartOfDigitalQueue(customer)
+  }
+
+  handleCustomerActionFromStartOfDigitalQueue(customer: Customer) {
+    const customerFirstActionFromDigitalQueue: State | EventState = returnResultBasedOnProb(StateFromReturnLaterQNProb)
+
+    switch (customerFirstActionFromDigitalQueue) {
+      case EventState.MISSED_QUEUE:
+        this.handleCustomerActionFromMissingQueue(customer)
+        break
+      case State.COUNTERS:
+        stationManager.appendCustomerToStationQueue(Station.COUNTERS, customer)
+        break
+    }
+  }
+
+  handleCustomerActionFromMissingQueue(customer: Customer) {
+    const customerActionFromMissingQueue: State = returnResultBasedOnProb(StateFromMissingQNProb)
+
+    switch (customerActionFromMissingQueue) {
+      case State.RETURN_LATER_QN:
+        this.appendCustomerToReturnLaterQueueNumberQueue(customer)
+        break
+      case State.EXIT:
+        updateCustomerDwellTimeAndExitSimulation(customer)
+        break
+    }
   }
 }
 
