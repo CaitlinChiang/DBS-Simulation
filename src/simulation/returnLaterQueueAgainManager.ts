b8497fbe7111183bc429dcab6914a7ea42a14daa
caitlin.ai/ <chiangcaitlin2003@gmail.com>
2024-03-26 14:46:43 +0800

    Constructed returnLaterQueueAgainManager

--- src/simulation/returnLaterQueueAgainManager.ts
+++ src/simulation/returnLaterQueueAgainManager.ts
@@ -1,5 +1,8 @@
 import { Customer } from '../types/customer'
 import { State } from '../enums/states'
+import { RETURN_LATER_QUEUE_AGAIN_RANGE } from '../enums/ranges'
+import { mainQueueManager } from './mainQueueManager'
+import { returnResultBasedOnRange } from '../utils/returnResultBasedOnRange'
 
 class ReturnLaterQueueAgainManager {
   private returnLaterQueueAgainQueue: Customer[] = []
@@ -8,7 +11,22 @@ class ReturnLaterQueueAgainManager {
 
   appendCustomerToReturnLaterQueueAgainQueue(customer: Customer) {
     customer.state = State.RETURN_LATER_QA
+    customer.qaDelayTime = returnResultBasedOnRange(RETURN_LATER_QUEUE_AGAIN_RANGE)
+
     this.returnLaterQueueAgainQueue.push(customer)
+  }
+
+  processCustomersFromReturnLaterQueueAgainQueue() {
+    const isReturnLaterQueueAgainQueueEmpty: boolean = this.returnLaterQueueAgainQueue.length === 0
+    if (isReturnLaterQueueAgainQueueEmpty) return
+
+    this.returnLaterQueueAgainQueue.sort((a, b) => (a.qaDelayTime! - b.qaDelayTime!))
+
+    const customer: Customer | any = this.returnLaterQueueAgainQueue.shift()
+
+    setTimeout(() => {
+      mainQueueManager.appendCustomerToMainQueue(customer)
+    }, customer.qaDelayTime)
   }
 }
 
